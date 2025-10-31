1320861d1e9c4cdff7485e33dc79a228f973f1be
Stiliyanss <stilianss03@gmail.com>
2025-10-31 12:17:28 +0200

    Fix post data

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -1,3 +1,5 @@
+import { use } from "react";
+
 const baseUrl = 'http://localhost:3030/jsonstore/users'
 
 export default {
@@ -9,12 +11,17 @@ export default {
   },
 
   async create(userData){
+      const {country, city, street, streetNumber, postData} = userData;
+      postData.address = {country, city, street, streetNumber};
+      postData.createdAt = new Date().toISOString();
+      postData.updatedAt = new Date().toISOString();
+
     const response = await fetch(baseUrl,{
       method:'POST',
       headers:{
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(userData)
+      body: JSON.stringify(postData)
     });
 
     const result = await response.json();
